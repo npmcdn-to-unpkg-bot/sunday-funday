180317870b1bfe5bddf58b513bef7e36c967a3c2
vladbogo <vladbogolin@gmail.com>
2016-01-09 20:45:49 +0200

    Added firstname and lastname to registration form

--- sundayfunday/forms/register.py
+++ sundayfunday/forms/register.py
@@ -4,7 +4,7 @@ from django.contrib.auth.forms import UserCreationForm
 class RegisterUserForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ('username', 'email',)
+        fields = ('username', 'first_name', 'last_name', 'email',)
 
     def __init__(self, *args, **kwargs):
         super(UserCreationForm, self).__init__(*args, **kwargs)
