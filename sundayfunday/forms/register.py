43c5c7e586572ceee12340538cd818612e404a2a
vladbogo <vladbogolin@gmail.com>
2016-01-09 20:32:00 +0200

    Partially fixed style

--- sundayfunday/forms/register.py
+++ sundayfunday/forms/register.py
@@ -5,3 +5,8 @@ class RegisterUserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email',)
+
+    def __init__(self, *args, **kwargs):
+        super(UserCreationForm, self).__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].widget.attrs.update({'class': 'form-control'})
