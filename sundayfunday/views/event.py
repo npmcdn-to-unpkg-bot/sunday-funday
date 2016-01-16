170396f8267051c1126efd71fcacd52445288adb
vladbogo <vladbogolin@gmail.com>
2016-01-16 23:10:08 +0200

    Attend event

--- sundayfunday/views/event.py
+++ sundayfunday/views/event.py
@@ -1,10 +1,14 @@
 from django.views.generic import CreateView
 from django.views.generic import DetailView
 from django.views.generic import UpdateView
+from django.http import HttpResponse
 
 from sundayfunday.forms.event import AddEventForm
 from sundayfunday.forms.event import UpdateEventForm
 from sundayfunday.models import Event
+from sundayfunday.models import AttendEvent
+from sundayfunday.models import User
+from django.utils import timezone
 
 class EventDetailView(DetailView):
     queryset = Event.objects.all()
@@ -13,6 +17,23 @@ class EventDetailView(DetailView):
     def get_object(self):
         object = super(EventDetailView, self).get_object()
         return object
+
+    def post(self, request, *args, **kwargs):
+        print(request.POST)
+        object = self.get_object()
+        attend_events = AttendEvent.objects. \
+            filter(user__id=int(request.POST['user'])).filter(event__id=object.id)
+        toggle = request.POST.get('toggle', None)
+        if toggle == None:
+            return HttpResponse("WRONG REQUEST")
+        if toggle == 'true':
+            if len(attend_events) == 0:
+                user = User.objects.get(id=int(request.POST['user']))
+                AttendEvent(user=user, event=object, data=timezone.now()).save()
+        if toggle == 'false':
+            if len(attend_events) > 0:
+                attend_events.delete()
+        return HttpResponse("OK")
 
 
 class AddEventView(CreateView):
--- sundayfunday/views/index.py
+++ sundayfunday/views/index.py
@@ -4,6 +4,7 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 from sundayfunday.models import Event
+from sundayfunday.models import AttendEvent
 from sundayfunday.models import Preference
 from sundayfunday.forms.event import FilterEventForm
 
@@ -19,6 +20,9 @@ class UserHomePageView(generic.View):
         pref = request.GET.getlist("preference", None)
         event_name = request.GET.get("event_name", "")
         relevant_events = Event.objects.all().distinct()
+        attended_events = AttendEvent.objects.filter(user__id=request.user.id)
+        attended_events = map(lambda x: x.event.id, attended_events)
+        print attended_events
 
         if pref:
             relevant_events = relevant_events.filter(preference__name__in=pref).distinct()
@@ -29,4 +33,5 @@ class UserHomePageView(generic.View):
                       context={'events' : relevant_events,
                                'preferences' : preferences,
                                'selected_preferences' : pref,
-                               'event_name_prefill' : event_name})
+                               'event_name_prefill' : event_name,
+                               'attended_events' : attended_events})
